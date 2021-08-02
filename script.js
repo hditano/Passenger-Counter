ccc7ae46a26fbcca40a76f03ea8a587b65c78393
Hernan Di Tano <hditano@gmail.com>
2021-08-02 06:33:06 -0300

    added save function functionality

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 let count = 0;
 let countEl = document.getElementById('id-counter');
+let saveEl = document.getElementById('save-el');
+
 
 function increment() {
     count = count + 1;
     countEl.innerText = count;
 
+}
+
+function save() {
+    let totalCount = count + ' -';
+    saveEl.innerText += totalCount;
+    console.log(count);
 }
