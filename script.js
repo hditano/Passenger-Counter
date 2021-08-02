ced1139389090c396acbdbc938f6ca67b635d6d3
Hernan Di Tano <hditano@gmail.com>
2021-08-02 02:27:43 -0300

    added increment function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("id-counter").innerText = 5;
+// document.getElementById("id-counter").innerText = 5;
+
+let count = 0;
+let countEl = document.getElementById('id-counter');
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+
+}
